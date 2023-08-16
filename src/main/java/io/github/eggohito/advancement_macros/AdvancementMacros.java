cef652f2b48ad89a173d95bfc611ed4a716967fc
eggohito <66972816+eggohito@users.noreply.github.com>
2023-08-16 15:12:33 +0800

    Rename main class To match the mod namespace

--- src/main/java/io/github/eggohito/advancement_macros/AdvancementMacros.java
+++ src/main/java/io/github/eggohito/advancement_macros/AdvancementMacros.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AdvancementMacro implements ModInitializer {
+public class AdvancementMacros implements ModInitializer {
 
 	public static final String MOD_NAMESPACE = "advancement-macros";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAMESPACE);
